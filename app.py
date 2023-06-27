584a9b16d7a8e55e3ececbfe7b0ef46ecce95422
Khushi <khushi.singhania01@gmail.com>
2023-06-27 21:13:52 +0530

    done

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/') #, methods=['GET'])
 def home():
     return render_template('index.html')
 def calculate_crop_yield():
-    
+
     field_area = float(request.form.get('field_area'))
     crop_yield_per_hectare = float(request.form.get('crop_yield_per_hectare'))
 
     total_yield = field_area * crop_yield_per_hectare
-    
+        
     print('Total crop yield is: ', total_yield)
-    
+        
     return jsonify({'crop_yield': total_yield})
+
 
 if __name__ == '__main__':
     app.run()
